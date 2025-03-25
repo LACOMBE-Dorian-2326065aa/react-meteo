cb974dfeaf01cd9771e9048a853a8e227958e1b7
LACOMBE-Dorian-2326065aa <dorian.lacombe@etu.univ-amu.fr>
2025-03-25 16:38:21 +0100

    3 onglets

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,7 +34,7 @@ export default function TabLayout() {
             }}
         />
         <Tabs.Screen
-            name="index2"
+            name="villes"
             options={{
                 title: 'Villes',
                 tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
